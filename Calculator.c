78072744110034f4f1da954e2feaa30aa297f152
Dhaval2008 <timbaldhiraj@gmail.com>
2026-09-13 12:32:59 +0530

    Implement basic calculator operations in Calculator.c

---
+++ Calculator.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+
+ int a,b;
+
+ printf("Enter your first number:");
+ scanf("%d",&a);
+
+ printf("Enter your number:");
+ scanf("%d",&b);
+
+ printf("Addition of Numbers is:%d\n",a+b);
+ 
+ printf("Multmultiplication of Numbers is:%d\n",a*b);
+ 
+ printf("Substaction of Numbers is:%d\n",a-b);
+ 
+ printf("Division of Numbers is:%d\n",a/b);
+ 
+ return 0;
+
+}
+
+
